47f043bb2b02e26431076b0b5e4d1fcc6494647a
luiz.pedro <luizpedrosousa64@gmail.com>
2023-02-26 22:05:27 -0300

    feat(components/links): add Google oauth link :sparkles:

---
+++ src/Client/src/lib/components/links/GoogleOauthLink.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import GoogleIcon from '$lib/components/icons/GoogleIcon.svelte';
+	export let className = '';
+	export let title: string;
+</script>
+
+<a
+	class="{className} flex h-16 w-full items-center justify-center rounded-md bg-white text-base font-semibold text-gray-500 hover:bg-gray-600 hover:no-underline "
+	href="/google"
+>
+	<span class="mr-2 h-6 w-6">
+		<GoogleIcon />
+	</span>
+	{title}
+</a>
